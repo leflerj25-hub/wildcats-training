40ba0f7e8f0865585332735f58b3729481de7a7e
leflerj25-hub <leflerj25@gmail.com>
2026-04-05 09:43:51 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Unregister and clear everything
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.matchAll({ includeUncontrolled: true }))
-      .then(clients => clients.forEach(c => c.navigate(c.url)))
-  );
-});
